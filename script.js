3350207f0cae63cf8ddb13a82e7bc047b16dae27
vanaja reddy <vanajareddyn277@gmail.com>
2024-11-02 20:53:35 +0530

    project

--- script.js
+++ script.js
@@ -20,7 +20,42 @@ window.onscroll = () => {
 }
 
 
+// swiper section code
 
+new Swiper('.card-wraper', {
+   
+  loop: true,
+  spaceBetween:30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints:{
+    0:{ 
+      slidesPerView: 1
+
+    },
+    768:{ 
+      slidesPerView: 2
+
+    },
+    1024:{ 
+      slidesPerView: 3
+
+    },
+  }
+});
 
 
 
